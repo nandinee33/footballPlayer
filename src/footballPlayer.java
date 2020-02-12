4f1608866c64b6fa63eebac25a3a8ae112287f53
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-12 07:34:21 +0530

    TAKE STRING INPUT FROM THE USER AND FIND ITS LENGTH

--- src/footballPlayer.java
+++ src/footballPlayer.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
 public class footballPlayer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int l = s.length();
+
+    }
+
 }
